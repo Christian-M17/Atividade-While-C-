ef109b0d6ffb9d0a012f4c5c3420aa7fa10b668a
Christian Ottero <62633531+MasterM17@users.noreply.github.com>
2022-06-27 11:41:01 -0300

    Visual Studio

---
+++ AtividadeWhile/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace AtividadeWhile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMultiplo3 = new System.Windows.Forms.Button();
+            this.btn100 = new System.Windows.Forms.Button();
+            this.btnAlunos = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMultiplo3
+            // 
+            this.btnMultiplo3.Location = new System.Drawing.Point(85, 75);
+            this.btnMultiplo3.Name = "btnMultiplo3";
+            this.btnMultiplo3.Size = new System.Drawing.Size(151, 23);
+            this.btnMultiplo3.TabIndex = 0;
+            this.btnMultiplo3.Text = "Multiplos de 3";
+            this.btnMultiplo3.UseVisualStyleBackColor = true;
+            this.btnMultiplo3.Click += new System.EventHandler(this.btnMultiplo3_Click);
+            // 
+            // btn100
+            // 
+            this.btn100.Location = new System.Drawing.Point(85, 134);
+            this.btn100.Name = "btn100";
+            this.btn100.Size = new System.Drawing.Size(151, 23);
+            this.btn100.TabIndex = 1;
+            this.btn100.Text = "100 a 1000";
+            this.btn100.UseVisualStyleBackColor = true;
+            this.btn100.Click += new System.EventHandler(this.btn100_Click);
+            // 
+            // btnAlunos
+            // 
+            this.btnAlunos.Location = new System.Drawing.Point(85, 194);
+            this.btnAlunos.Name = "btnAlunos";
+            this.btnAlunos.Size = new System.Drawing.Size(151, 23);
+            this.btnAlunos.TabIndex = 2;
+            this.btnAlunos.Text = "Alunos";
+            this.btnAlunos.UseVisualStyleBackColor = true;
+            this.btnAlunos.Click += new System.EventHandler(this.btnAlunos_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 435);
+            this.Controls.Add(this.btnAlunos);
+            this.Controls.Add(this.btn100);
+            this.Controls.Add(this.btnMultiplo3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMultiplo3;
+        private System.Windows.Forms.Button btn100;
+        private System.Windows.Forms.Button btnAlunos;
+    }
+}
+
---
+++ AtividadeWhile/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtividadeWhile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMultiplo3_Click(object sender, EventArgs e)
+        {
+            int num = 1;
+
+            while (num <= 99)
+            {
+                if (num % 3 == 0)
+                {
+                    MessageBox.Show(num + " É multiplo de 3");
+                }
+                num = num + 1;
+            }
+        }
+
+        private void btn100_Click(object sender, EventArgs e)
+        {
+            int num = 100;
+
+                while (num <= 1000)
+            {
+                MessageBox.Show("Número" + num);
+                num = num + 1;
+            }
+        }
+
+        private void btnAlunos_Click(object sender, EventArgs e)
+        {
+            int aluno = 1;
+            int nota;
+
+            while (aluno <= 5)
+            {
+                nota = aluno * 10;
+                MessageBox.Show("Aluno " + aluno + ":" + nota);
+                aluno = aluno + 1;
+            }
+        }
+        
+    }
+}
